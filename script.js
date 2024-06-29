d47e0f1210b17890345adab46c725c9081444fa9
Adeola Fabumuyi <adeolafab11@gmail.com>
2024-06-29 13:12:08 +0100

    Fixed some bugs

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 let read = document.querySelector(".wel p")
 console.log(read)
 let unreadenotifications = document.querySelectorAll(".blue")
+let red = document.querySelectorAll(".reddot")
 read.addEventListener("click", ()=> {
     console.log(unreadenotifications)
     unreadenotifications.forEach((notification) => {
-        console.log(notification)
-        notification.style.backgroundColor= "white"
+    console.log(notification)
+    notification.style.backgroundColor= "white"
     })
-     let backgroundColor= "blue"
-    // Do it here
-    // the_variable_name.style.display = "none"
+     red.forEach((unread) => {
+        console.log(unread)
+        unread.style.display ="none"
+     })
 })
-let red = document.querySelector(".MW blue .circle")
-let rede = document.querySelector(".AG blue .cir")
-let reda = document.querySelector(".JT blue .cool")
